5dc71c30f31450dbe1cb90ce256df890691cf63e
裴子栋 <peizidong@mail.tsinghua.edu.cn>
2023-01-18 11:28:15 +0800

    add draw scropt

---
+++ draw_chart/cluster_kmean.py
@@ -0,0 +1,248 @@
+import sys
+
+import matplotlib.pyplot as plt
+from draw_chart.generator_rank_json import generator_rank_json_by_topo_type
+import os
+import json
+from pylab import mpl
+from sklearn.cluster import KMeans
+import numpy as np
+
+c2 = ['asRank']
+
+# plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
+# plt.rcParams['axes.unicode_minus'] = False  # 这两行需要手动设置
+mpl.rcParams['font.family'] = 'times'
+# mpl.rcParams['figure.constrained_layout.use'] = True
+
+
+# cm = {}
+#
+# with open('country_map.json', 'r') as cmf:
+#     cm = json.load(cmf)
+
+mark_list = ['+', 's', 'o', 'P', 'x', '.', 'D', 'D', ]
+facecolors_list = ['black', 'none', 'none', 'none', 'black', 'black', 'black', 'black', ]
+color_list = ['red', 'green', 'black', 'blue', 'orange', 'pink', 'purple']
+
+title_font_size = 25
+label_font_size = 22
+table_font_size = 20
+row0_font_size = 20
+n_clusters = 6
+
+def make_result(path, file_name, _type, result):
+    real_path = os.path.join(path, file_name)
+
+    with open(real_path) as f:
+        all_data = json.load(f)
+        for cc in all_data:
+            start_index = 0
+            lens = len(all_data[cc])
+            end_index = lens
+            if _type == 'basic':
+                end_index = int((lens / 3) * 1)
+            elif _type == 'user':
+                start_index = int((lens / 3) * 1)
+                end_index = int((lens / 3) * 2)
+            elif _type == 'domain':
+                start_index = int((lens / 3) * 2)
+            if cc not in result.keys():
+                result[cc] = [0, 0]
+            if file_name == 'med_rank.json':
+                result[cc][0] = sum(all_data[cc][start_index:end_index]) * 4
+            if file_name == 'var_rank.json':
+                result[cc][1] = sum(all_data[cc][start_index:end_index])
+
+
+def get_all_num(cc_as):
+    cc = cc_as.split('-')[0]
+    with open('ccInternal/cc2as/%s.json' % cc, 'r') as f:
+        as_list = json.load(f)
+        return len(as_list)
+
+
+def get_as_num(path, cc_as, _dir):
+    cc_and_as = cc_as.split('-')
+    if _dir != 'var':
+        cc_and_as[1] = 'dcomplete%s' % cc_and_as[1]
+    if os.path.exists('%s/count_num/%s/%s.json' % (path, cc_and_as[0], cc_and_as[1])):
+        with open('%s/count_num/%s/%s.json' % (path, cc_and_as[0], cc_and_as[1]), 'r') as f:
+            data = json.load(f)
+            return int(data['%s' % cc_and_as[1]]['asNum'])
+    return 0
+
+
+def make_result_by_rank(path, _type):
+    real_result = {}
+    topo_list = c2
+    result = generator_rank_json_by_topo_type(path, save=False)['result']
+    for topo in topo_list:
+        t = result['%s-%s' % (_type, topo)]
+        for k in t:
+            if k not in real_result:
+                real_result[k] = [0, 0]
+            real_result[k][0] += t[k][0]
+            real_result[k][1] += t[k][1]
+
+    return real_result
+
+
+def make_result_by_rank_all(path):
+    real_result = {}
+    topo_list = c2
+    result = generator_rank_json_by_topo_type(path, save=False)['result']
+    for topo in topo_list:
+        for _type in ['basic', 'user', 'domain']:
+            t = result['%s-%s' % (_type, topo)]
+            for k in t:
+                if k not in real_result:
+                    real_result[k] = [0, 0]
+                real_result[k][0] += t[k][0]
+                real_result[k][1] += t[k][1]
+
+    return real_result
+
+
+def draw_plot(_type, path, position, title):
+    estimator = KMeans(n_clusters=n_clusters)  # 构造聚类器
+
+    result = {}
+    dir_path = path
+    # USE RANK
+    result = make_result_by_rank(dir_path, _type)
+
+    # USE WEIGHT
+    # make_result(dir_path, 'med_rank.json', _type, result)
+    # make_result(dir_path, 'var_rank.json', _type, result)
+
+    ax = fig.add_subplot(position)
+    ax.set_title(title, fontsize=title_font_size)
+    # fig.subplots_adjust(right=0.8)
+    X = list(map(lambda x: [result[x][0], result[x][1], x], result))
+    X.sort(key=lambda x: float(x[0]) + float(x[1]))
+    # X1 = list(map(lambda x: [x[0], x[1]], result.values()))
+    X = np.array(X)
+    estimator.fit(X[:, :2])
+    label_pred = estimator.labels_
+    table_data = []
+    table_index = -1
+    old_class_index = {}
+    a_map = {}
+    print(label_pred)
+    for index, data in enumerate(X):
+        # print(label_pred[index], data[2])
+        if label_pred[index] not in old_class_index:
+            table_index += 1
+            old_class_index[label_pred[index]] = table_index
+        using_table_index = old_class_index[label_pred[index]]
+
+        if len(table_data) == using_table_index:
+            table_data.append(['Class%s' % (using_table_index + 1)])
+        a = ax.scatter(float(data[0]), float(data[1]), marker=mark_list[using_table_index], facecolors=facecolors_list[using_table_index],
+                       s=100, color="black")
+        if mark_list[using_table_index] not in a_map:
+            a_map[mark_list[using_table_index]] = a
+
+        table_data[using_table_index].append(data[2])
+    ax.legend(handles=list(a_map.values()), labels=list(map(lambda x: x[0], table_data)), bbox_to_anchor=(0.99, 0.02), loc='lower right',
+              labelspacing=1.5,
+              borderaxespad=0., prop={'size': 15, 'family': 'times'})
+    max_len = max(map(lambda x: len(x), table_data))
+
+    for index, i in enumerate(table_data):
+        if len(i) < max_len:
+            i += [''] * (max_len - len(i))
+
+    table = ax.table(cellText=table_data, bbox=[0.0, -0.45, 1, 0.35], cellLoc='center')
+    cells = table.properties()["celld"]
+    table.auto_set_font_size(False)
+    table.set_fontsize(table_font_size)
+    # table.scale(1, 4)
+    for i in range(0, n_clusters):
+        cells[i, 0].set_fontsize(row0_font_size)
+        # for j in range(0,max_len):
+        #     cells[i,j].set_height(0.1)
+
+    ax.add_table(table)
+
+    ax.tick_params(axis='both', which='major', labelsize=label_font_size)
+
+
+def draw_plot_all(_type, path, position, title):
+
+    estimator = KMeans(n_clusters=n_clusters)  # 构造聚类器
+
+    result = {}
+    dir_path = path
+    # USE RANK
+    result = make_result_by_rank_all(dir_path)
+
+    # USE WEIGHT
+    # make_result(dir_path, 'med_rank.json', _type, result)
+    # make_result(dir_path, 'var_rank.json', _type, result)
+
+    ax = fig.add_subplot(position)
+    ax.set_title(title, fontsize=title_font_size * 1.5)
+    # fig.subplots_adjust(right=0.8)
+    X = list(map(lambda x: [result[x][0], result[x][1], x], result))
+    X.sort(key=lambda x: float(x[0]) + float(x[1]))
+    # X1 = list(map(lambda x: [x[0], x[1]], result.values()))
+    X = np.array(X)
+    estimator.fit(X[:, :2])
+    label_pred = estimator.labels_
+    table_data = []
+    table_index = -1
+    old_class_index = {}
+    a_map = {}
+    # print(label_pred)
+    for index, data in enumerate(X):
+        # print(label_pred[index], data[2])
+        if label_pred[index] not in old_class_index:
+            table_index += 1
+            old_class_index[label_pred[index]] = table_index
+        using_table_index = old_class_index[label_pred[index]]
+
+        if len(table_data) == using_table_index:
+            table_data.append(['Class%s' % (using_table_index + 1)])
+        a = ax.scatter(float(data[0]), float(data[1]), marker=mark_list[using_table_index], facecolors=facecolors_list[using_table_index],
+                       s=100, color="black")
+        if mark_list[using_table_index] not in a_map:
+            a_map[mark_list[using_table_index]] = a
+
+        table_data[using_table_index].append(data[2])
+    ax.legend(handles=list(a_map.values()), labels=list(map(lambda x: x[0], table_data)), bbox_to_anchor=(0.99, 0.02), loc='lower right',
+              labelspacing=1.5,
+              borderaxespad=0., prop={'size': 15, 'family': 'times'})
+    max_len = max(map(lambda x: len(x), table_data))
+
+    for index, i in enumerate(table_data):
+        if len(i) < max_len:
+            i += [''] * (max_len - len(i))
+
+    table = ax.table(cellText=table_data, bbox=[0.0, -0.45, 1, 0.35], cellLoc='center')
+    cells = table.properties()["celld"]
+    table.auto_set_font_size(False)
+    table.set_fontsize(table_font_size)
+    # table.scale(1, 4)
+    for i in range(0, n_clusters):
+        cells[i, 0].set_fontsize(row0_font_size)
+        # for j in range(0,max_len):
+        #     cells[i,j].set_height(0.1)
+
+    ax.add_table(table)
+
+    ax.tick_params(axis='both', which='major', labelsize=label_font_size)
+
+
+if __name__ == '__main__':
+    fig = plt.figure(figsize=(30, 10))  # 创建画布
+    draw_plot('basic', os.path.join(sys.argv[1], 'output', 'public'), 131, 'clustering results(connectivity)')
+    print('------------')
+    draw_plot('user', os.path.join(sys.argv[1], 'output', 'public'), 132, 'clustering results(user)')
+    print('------------')
+    draw_plot('domain', os.path.join(sys.argv[1], 'output', 'public'), 133, 'clustering results(domain)')
+    # print('------------')
+    # draw_plot_all('', '../public', 111, 'clustering results of Inter-region survivability')
+    # plt.tight_layout()
+    plt.show()
---
+++ draw_chart/generator_rank_json.py
@@ -0,0 +1,237 @@
+import os
+import json
+import sys
+
+c1 = ['basic', 'user', 'domain']
+c2 = ['asRank', 'problink', 'toposcope', 'toposcope_hidden']
+
+# rank原始文件里面有，但是不显读的时候，写在skip_list里面
+skip_list = []
+# 用来区分优化前和优化后的结果
+suffix = ''
+
+__type_map = {
+    'med': 0,
+    'var': 1
+}
+
+SKIP_URB = False
+
+
+def generator_type_map():
+    result = {}
+    for i in range(len(c1) * len(c2)):
+        if c1[int(i / 4)] in skip_list or c2[int(i % 4)] in skip_list:
+            continue
+
+        result[i] = '%s-%s%s' % (c1[int(i / 4)], c2[int(i % 4)], suffix)
+
+    return result
+
+
+def generator_type_map_by_num(num):
+    result = {}
+    for i in range(len(c1) * len(c2)):
+        if c1[int(i / 4)] in skip_list or c2[int(i % 4)] in skip_list:
+            continue
+
+        result[i] = '%s-%s.%s' % (c1[int(i / 4)], c2[int(i % 4)], num)
+
+    return result
+
+
+topo_type_map = generator_type_map()
+
+
+# print(topo_type_map)
+
+
+# def cal_rank_sorted(type_index, all_data):
+#     sort_list = []
+#     for cc in all_data:
+#         if len(all_data[cc]) == 0:
+#             continue
+#         if len(all_data[cc]) <= type_index:
+#             sort_list.append([cc, 10000])
+#         else:
+#             sort_list.append([cc, all_data[cc][type_index]])
+#     sort_list.sort(key=lambda x: x[1])
+#     return sort_list
+
+
+def make_result(path, file_name, result, __type):
+    real_path = os.path.join(path, file_name)
+
+    with open(real_path) as f:
+        all_data = json.load(f)
+        sort_dict = {}
+
+        for cc in all_data:
+            for rank_value in all_data[cc]:
+                if rank_value not in sort_dict:
+                    sort_dict[rank_value] = []
+                if rank_value not in result:
+                    result[rank_value] = {}
+                sort_dict[rank_value].append([cc, all_data[cc][rank_value]])
+
+        for k in sort_dict:
+            sort_dict[k].sort(key=lambda x: x[1])
+
+            rank = 0
+            min_v = -1
+
+            for cl in sort_dict[k]:
+                if cl[0] not in result[k].keys():
+                    result[k][cl[0]] = [0, 0]
+                if min_v < cl[1]:
+                    rank += 1
+                    min_v = cl[1]
+                result[k][cl[0]][__type_map[__type]] = rank
+            # print('cc = %s,rank = %s,weight = %s,type = %s' % (cl[0], rank, cl[1], __type))
+
+
+def make_result_by_cc(path, file_name, result, __type):
+    real_path = os.path.join(path, file_name)
+    with open(real_path) as f:
+        all_data = json.load(f)
+    sort_dict = {}
+
+    for cc in all_data:
+        for rank_value in all_data[cc]:
+            if rank_value not in sort_dict:
+                sort_dict[rank_value] = []
+            sort_dict[rank_value].append([cc, all_data[cc][rank_value]])
+
+    for k in sort_dict:
+        sort_dict[k].sort(key=lambda x: x[1])
+
+        rank = 0
+        min_v = -1
+
+        for cl in sort_dict[k]:
+
+            if cl[0] not in result:
+                result[cl[0]] = {}
+            if min_v < cl[1]:
+                rank += 1
+                min_v = cl[1]
+            # if SKIP_URB:
+            #     if cl[0] in ['US', 'RU', 'BR']:
+            #         continue
+            if k not in result[cl[0]]:
+                result[cl[0]][k] = [0, 0]
+            result[cl[0]][k][__type_map[__type]] = rank
+        # print('cc = %s,rank = %s,weight = %s,type = %s' % (cl[0], rank, cl[1], __type))
+
+
+def make_result_by_cc_only_cc(path, file_name, result, __type, cc3, index):
+    # topo_type_map2 = generator_type_map_by_num(index)
+    real_path = os.path.join(path, file_name)
+    with open(real_path) as f:
+        all_data = json.load(f)
+        sort_dict = {}
+
+        for cc in all_data:
+            for rank_value in all_data[cc]:
+                sort_dict_key = f'{rank_value}.{index}'
+                if sort_dict_key not in sort_dict:
+                    sort_dict[sort_dict_key] = []
+                sort_dict[sort_dict_key].append([cc, all_data[cc][rank_value]])
+
+        for k in sort_dict:
+            sort_dict[k].sort(key=lambda x: x[1])
+            # print(sort_dict[k],cc3,k)
+            rank = 0
+            min_v = -1
+
+            for cl in sort_dict[k]:
+                # if __type == 'var':
+                #     print(cl,cc3)
+                if cl[0] not in result and cl[0] == cc3:
+                    result[cl[0]] = {}
+                if min_v < cl[1]:
+                    rank += 1
+                    min_v = cl[1]
+                # print(min_v,rank,cl)
+                # if __type == 'var':
+                #     print('min_v',min_v)
+
+                if cl[0] != cc3:
+                    continue
+                # 'HU', 4.974242424242425
+                if k not in result[cl[0]]:
+                    result[cl[0]][k] = [0, 0]
+                result[cl[0]][k][__type_map[__type]] = rank
+
+            # print('cc = %s,rank = %s,weight = %s,type = %s' % (cl[0], rank, cl[1], __type))
+
+
+def generator_rank_json_by_topo_type(path, file_name="rank_by_topo_type", save=True):
+    result = {}
+    make_result(path, 'med_rank.json', result, 'med')
+    make_result(path, 'var_rank.json', result, 'var')
+    dst_path = os.path.join(os.getcwd(), 'rank_files')
+    os.makedirs(dst_path, exist_ok=True)
+    if save:
+        with open(os.path.join(dst_path, '%s%s.json' % (file_name, suffix)), 'w') as f:
+            json.dump(result, f)
+    return {'result': result,
+            'name': '%s%s.json' % (file_name, suffix)}
+
+
+def generator_rank_json_by_cc(path, file_name="rank_by_cc", save=True):
+    result = {}
+    make_result_by_cc(path, 'med_rank.json', result, 'med')
+    make_result_by_cc(path, 'var_rank.json', result, 'var')
+    dst_path = os.path.join(os.getcwd(),'rank_files')
+    os.makedirs(dst_path,exist_ok=True)
+    if save:
+        with open(os.path.join(dst_path, '%s%s.json' % (file_name, suffix)), 'w') as f:
+            json.dump(result, f)
+    return {'result': result,
+            'name': '%s%s.json' % (file_name, suffix)}
+
+
+def generator_rank_json_by_cc_loop_cc(path, cc, file_name="rank_by_cc", save=True):
+    # if cc in ['US', 'BR', 'RU']:
+    #     return
+
+    # if cc in ['IN', 'ID', 'HU', 'PT']:
+    #     return
+
+    opt_num_set = set()
+    files = os.listdir(path)
+    dst_path = os.path.join(os.getcwd(), 'rank_files')
+    os.makedirs(dst_path, exist_ok=True)
+    for i in files:
+        opt_num = i.split('.')[-2]
+        opt_num_set.add(opt_num)
+    for i in list(opt_num_set):
+        result = {}
+        if not os.path.exists(os.path.join(path, 'med_rank.%s.%s.json' % (cc, i))):
+            continue
+        make_result_by_cc_only_cc(path, 'med_rank.%s.%s.json' % (cc, i), result, 'med', cc, i)
+        make_result_by_cc_only_cc(path, 'var_rank.%s.%s.json' % (cc, i), result, 'var', cc, i)
+
+        if save:
+            with open(os.path.join(dst_path, '%s.%s.%s.json' % (file_name, cc, i)),
+                      'w') as f:
+                json.dump(result, f)
+
+
+if __name__ == '__main__':
+    # print(sys.argv[1])
+    '''
+    运行 python3 generator_rank_json.py 运行时路径
+    '''
+    public_path2 = os.path.join(sys.argv[1], 'output', 'public')
+    public_path = os.path.join(public_path2, 'optimize')
+    generator_rank_json_by_cc(public_path2)
+
+    cc_list = ["LV", "HU", "CL", "SK", "PT", "FI", "CO", "TR", "PH", "NG", "DK", "MX", "LU", "KH", "TW", "TH", "KR", "NO", "MY", "CN", "BG", "IR",
+               "IE", "AR", "RO", "NZ", "CZ", "BD", "SE", "UA", "SG", "HK", "ES", "AT", "JP", "IN", "ID", "CH", "PL", "NL", "IT", "FR", "CA", "ZA",
+               "AU", "GB", "DE", "US", "RU", "BR"]
+    # cc_list = ['UA','AR','BD','RO','NL','CH','BG','CN','CZ','AT','TH','NZ','SG','CL','PH','NO','LV','MX','SK','FI','NG','CO']
+    for cc2 in cc_list:
+        generator_rank_json_by_cc_loop_cc(public_path, cc2)
+    # generator_rank_json_by_cc_loop_cc(public_path, 'LV')
